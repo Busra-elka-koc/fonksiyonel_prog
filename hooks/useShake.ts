6ae12a480abc736db4a4101cd3c82b81155fd04d
Busra-elka-koc <157537412+Busra-elka-koc@users.noreply.github.com>
2025-04-19 20:48:09 +0300

    hooks

---
+++ hooks/useShake.ts
@@ -0,0 +1,33 @@
+import { useRef } from 'react'
+import { Animated } from 'react-native'
+
+export const useShake = () => {
+  const shakeAnimation = useRef(new Animated.Value(0)).current
+
+  const shake = () => {
+    Animated.sequence([
+      Animated.timing(shakeAnimation, {
+        toValue: 10,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(shakeAnimation, {
+        toValue: -10,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(shakeAnimation, {
+        toValue: 10,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(shakeAnimation, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start()
+  }
+
+  return { shakeAnimation, shake }
+}
